761fcc460bf0c30db6368114e6edcf7bc484188b
CristianEstebanCano <canox09@gmail.com>
2025-02-14 14:05:40 -0500

    operaciones basicas

--- src/Main.kt
+++ src/Main.kt
@@ -3,4 +3,9 @@ fun main() {
     println("Cristian Esteban Cano Vega")
     println("Montar moto")
 
+    println("Esta es mi rama de desarrollo")
+    println("SUMA")
+    println("Resta")
+    println("Multiplicacion")
+    println("division")
 }
